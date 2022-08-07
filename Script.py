f93ea995cccb771db2f9b3014f765adc34d25784
Happy9012 <110512707+Happy9012@users.noreply.github.com>
2022-08-07 17:09:45 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -4,24 +4,24 @@ class script(object):
     HELP_TXT = """𝙷𝙴𝚈 {}
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂."""
     ABOUT_TXT = """✯ 𝙼𝚈 𝙽𝙰𝙼𝙴: {}
-✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/KGN_OFFICIAL>ZAHID</a>
+✯ 𝙲𝚁𝙴𝙰𝚃𝙾𝚁: <a href=https://t.me/Himanshurathore0>DON</a>
 ✯ 𝙻𝙸𝙱𝚁𝙰𝚁𝚈: 𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼
 ✯ 𝙻𝙰𝙽𝙶𝚄𝙰𝙶𝙴: 𝙿𝚈𝚃𝙷𝙾𝙽 𝟹
 ✯ 𝙳𝙰𝚃𝙰 𝙱𝙰𝚂𝙴: 𝙼𝙾𝙽𝙶𝙾 𝙳𝙱
 ✯ 𝙱𝙾𝚃 𝚂𝙴𝚁𝚅𝙴𝚁: 𝙷𝙴𝚁𝙾𝙺𝚄
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
-- Mahesh Babu is not a open source project but if you want it repo or bot then contact @KGN_OWNER_BOT. 
-- Source - @KGN_OWNER_BOT  
+- Bunny is not a open source project but if you want it repo or bot then contact @KGN_OWNER_BOT. 
+- Source - @Himanshurathore0 
 
 <b>DEV:</b>
-- <a href=https://t.me/KGN_OFFICIAL>ZAHID</a>"""
+- <a href=https://t.me/Himanshurathore0>DON</a>"""
     MANUELFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and Mahesh will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
-1. Mahesh should have admin privillage.
+1. Bunny should have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
 
@@ -40,7 +40,7 @@ class script(object):
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:https://t.me/MAHESH_BABU_BOT)</code>
+<code>[Button Text](buttonurl:https://t.me/BunnyMoviebot)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
